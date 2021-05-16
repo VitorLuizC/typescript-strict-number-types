deab06348d952885a8596da13d2e9d4d13a36bb1
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-05-16 01:34:19 -0300

    ✨ Create Infinity type

---
+++ src/types/Infinity.ts
@@ -0,0 +1,6 @@
+import type Brand from './Brand';
+
+/** A brand type that represents 'Infinity' and '-Infinity' values. */
+type Infinity = Brand<number, 'Infinity'>;
+
+export default Infinity;
--- src/types/NaN.ts
+++ src/types/NaN.ts
@@ -1,6 +1,6 @@
 import type Brand from './Brand';
 
-/** A brand type that represents the `NaN` value. */
+/** A brand type that represents the 'NaN' value. */
 type NaN = Brand<number, 'NaN'>;
 
 export default NaN;

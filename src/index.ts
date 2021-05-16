1ae8699e68b1f22da08536d895f3de6b9f1b3d58
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-05-16 02:04:15 -0300

    ✨ Export types and type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,9 @@
-/**
- * A Branded Type for values parseable to number.
- */
-export type NumberParseable = (number | string | boolean) & {
-  readonly isNumberParseble: unique symbol;
-};
+import './isFinite';
+import './isNaN';
+import './parseFloat';
+import './parseFloat';
 
-/**
- * Check if value is parseable to number.
- * @example ```ts
- * isNumberParseable('AAAA');
- * //=> false
- *
- * isNumberParseable('100');
- * //=> true
- *
- * if (!isNumberParseable(value))
- *   throw new Error('Value can\'t be parseable to `Number`.')
- * return Number(value);
- * ```
- * @param value - An `unknown` value to be checked.
- */
-export const isNumberParseable = (value: unknown): value is NumberParseable =>
-  !Number.isNaN(Number(value));
+export type { default as Float } from './types/Float';
+export type { default as Infinity } from './types/Infinity';
+export type { default as Integer } from './types/Integer';
+export type { default as NaN } from './types/NaN';
